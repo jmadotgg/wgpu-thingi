c6f4ef819fda97e1bf8f47147073a787140a7c9a
Julius Makel - I20 <julius.makel@nordakademie.de>
2023-10-16 20:54:06 +0200

    It's a cube now!!!

--- src/lib.rs
+++ src/lib.rs
@@ -415,19 +415,24 @@ impl Vertex {
 
 #[rustfmt::skip]
 const VERTICES_OLD: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
-];
-
-#[rustfmt::skip]
-const VERTICES: &[Vertex] = &[
     Vertex { position: [-0.5, 0.5, 0.0], color: [0.5, 0.0, 0.0] }, // A
     Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 0.5, 0.0] }, // B
     Vertex { position: [0.5, 0.5, 0.0], color: [0.0, 0.0, 0.5] }, // C
     Vertex { position: [0.5, -0.5, 0.0], color: [0.5, 0.0, 0.0] }, // D
+];
+
+#[rustfmt::skip]
+const VERTICES: &[Vertex] = &[
+    // Front
+    Vertex { position: [-1.0, 1.0, 0.0], color: [1.0, 0.0, 0.0] }, // A
+    Vertex { position: [-1.0, -1.0, 0.0], color: [0.0, 1.0, 0.0] }, // B
+    Vertex { position: [1.0, 1.0, 0.0], color: [0.0, 0.0, 1.0] }, // C
+    Vertex { position: [1.0, -1.0, 0.0], color: [1.0, 0.0, 0.0] }, // D
+    // Back
+    Vertex { position: [-1.0, 1.0, -2.0], color: [1.0, 0.0, 0.0] }, // A
+    Vertex { position: [-1.0, -1.0, -2.0], color: [0.0, 1.0, 0.0] }, // B
+    Vertex { position: [1.0, 1.0, -2.0], color: [0.0, 0.0, 1.0] }, // C
+    Vertex { position: [1.0, -1.0, -2.0], color: [1.0, 0.0, 0.0] }, // D
 ];
 
 #[rustfmt::skip]
@@ -439,8 +444,24 @@ const INDICES_OLD: &[u16] = &[
 
 #[rustfmt::skip]
 const INDICES: &[u16] = &[
+    // front
     0, 1, 2,
-    1, 3, 2
+    1, 3, 2,
+    // back
+    6, 5, 4,
+    5, 6, 7,
+    // left
+    0, 4, 1,
+    1, 4, 5,
+    // right
+    6, 2, 3,
+    3, 7, 6,
+    // top
+    0, 2, 4,
+    6, 4, 2,
+    // bottom
+    1, 5, 3,
+    5, 7, 3
 ];
 
 #[repr(C)]
